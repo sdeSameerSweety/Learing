90ca2319ecbf2c8c2523657100a2000e16db8342
unknown <sameerswain.gse.23@outlook.com>
2023-07-21 23:25:51 +0530

    added function to store Id Data in signUp.js file

---
+++ signUp.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const fname = document.getElementById('fname').value;
+            const lname = document.getElementById('lname').value;
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            
+        });
