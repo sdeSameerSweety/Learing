8538fba514f0a4860888895cb29a3f63c6854803
unknown <sameerswain.gse.23@outlook.com>
2023-07-21 23:27:04 +0530

    added function to post frontend data to server in signUp.js file

--- signUp.js
+++ signUp.js
@@ -5,5 +5,21 @@ const form = document.querySelector('form');
             const lname = document.getElementById('lname').value;
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
-            
+            // TODO: Send data to server
+            const data = {fname, lname, email, password};
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data),
+            };
+            fetch('/api', options).then(response => {
+                console.log(response);
+                if (response.status === 200) {
+                    window.location.href = '/login';
+                }
+            }
+            );
+
         });
